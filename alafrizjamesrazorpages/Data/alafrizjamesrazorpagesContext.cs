565ec75120a390ea8ed08bcbc5affb115326640f
TECH4ED-PC5 <TECH4ED-PC5@TECH4ED_5>
2022-03-23 13:58:10 +0800

    Add project files.

---
+++ alafrizjamesrazorpages/Data/alafrizjamesrazorpagesContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using alafrizjamesrazorpages.Pages.Basketball;
+
+namespace alafrizjamesrazorpages.Data
+{
+    public class alafrizjamesrazorpagesContext : DbContext
+    {
+        public alafrizjamesrazorpagesContext (DbContextOptions<alafrizjamesrazorpagesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<alafrizjamesrazorpages.Pages.Basketball.Class> Class { get; set; }
+    }
+}
---
+++ alafrizjamesrazorpages/Pages/Comedy/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using alafrizjamesrazorpages.Data;
+
+namespace alafrizjamesrazorpages.Pages.Basketball
+{
+    public class IndexModel : PageModel
+    {
+        private readonly alafrizjamesrazorpages.Data.alafrizjamesrazorpagesContext _context;
+
+        public IndexModel(alafrizjamesrazorpages.Data.alafrizjamesrazorpagesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Class> Class { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Class = await _context.Class.ToListAsync();
+        }
+    }
+}
